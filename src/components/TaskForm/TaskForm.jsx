75ce8f205461784d7f92096ac02ab29d41c4169e
Vikas Pal <vikaspal@Vikass-MacBook-Air.local>
2025-08-25 23:54:40 +0530

    Added form handler in Input/Dropdown

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,11 +1,24 @@
 import "./TaskForm.css"
 import Tag from '../TagButton/Tag'
+import { useState } from "react"
 
 function TaskForm() {
+
+    const [input , setInput] = useState("");
+    const [status, setStatus] = useState("");
+    function handleInput(e)
+    {
+        console.log(e.target.value)
+    }
+    function handleStatus(e)
+    {
+        console.log(e.target.value)
+    }
+
   return (
     <div className='app_hedaer'>
                 <form action="" className='form_data'>
-                    <input type="text" placeholder='Enter task details' />
+                    <input type="text" placeholder='Enter task details' onChange={handleInput} />
                     <div className="jira_statusButton">
                         <div className="status_part1">
                             <Tag tagName='DEV'/>
@@ -13,7 +26,7 @@ function TaskForm() {
                             <Tag tagName='Product Owner'/>
                         </div>
                         <div className="status_part1">
-                            <select name="" className="task_status tag_button">
+                            <select name="" className="task_status tag_button" onChange={handleStatus}>
                                 <option value="Read for Development">Read for Development</option>
                                 <option value="In Progress">In Progress</option>
                                 <option value="Ready for test">Ready for test</option>
